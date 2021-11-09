442d9f00c5d1457a832c3d86c8870fe215ce951f
Julia Nesterenko <90193842+nesterenkojul@users.noreply.github.com>
2021-11-09 21:01:53 +0300

    Используем модуль - функцию конструктора

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const os = require('os')
-const greeting = require('./greeting')
+const User = require('./user.js')
 
-// получим имя текущего пользователя
-let userName = os.userInfo().username
-
-console.log(`Дата запроса: ${greeting.date}`)
-console.log(greeting.getMessage(userName))
+let julia = new User('Julia', 18)
+julia.sayHi()
