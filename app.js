49c43b928d304d78e2411896d17d5cce10dc2a93
Julia Nesterenko <90193842+nesterenkojul@users.noreply.github.com>
2021-11-09 20:56:05 +0300

    Используем созданный модуль

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-const http = require('http')
-http
-  .createServer(function (request, response) {
-    response.end('Hello NodeJS!')
-  })
-  .listen(3000, '127.0.0.1', function () {
-    console.log(
-      'Сервер начал прослушивание запросов на порту 3000'
-    )
-  })
+const os = require('os')
+const greeting = require('./greeting')
+
+// получим имя текущего пользователя
+let userName = os.userInfo().username
+
+console.log(`Дата запроса: ${greeting.date}`)
+console.log(greeting.getMessage(userName))
